c791a7bea764bf6a4bcf5cf1f006552a5d9c2439
Lucas Mondaca <lucas.mondaca@hotmail.com>
2026-07-12 16:50:27 -0400

    se reemplaza tematica monke to skeleton y se agrega validacion de login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  usuario = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.usuario = params['usuario'] ?? '';
+    });
+  }
 
 }
